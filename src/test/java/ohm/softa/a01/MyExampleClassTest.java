8e2c786b61b2652b9c0a82d0762573dbbceab3ab
schwabauermi <schwabauer.mi@gmail.com>
2019-03-28 10:28:16 +0100

    loesung

--- src/test/java/ohm/softa/a01/MyExampleClassTest.java
+++ src/test/java/ohm/softa/a01/MyExampleClassTest.java
@@ -10,6 +10,8 @@ class MyExampleClassTest {
     @Test
     void testMultiply() {
         IntStream.range(-5, 5)
-                .forEach(i -> assertEquals(i * (i - 1), new MyExampleClass().multiply(i, i - 1)));
+                .forEach(i ->
+                        assertEquals(i * (i - 1),
+                                new MyExampleClass().multiply(i, i - 1)));
     }
 }
